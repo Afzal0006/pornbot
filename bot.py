fdbc9a7bb2a9fa84083940a467106c547ade283e
Afzal0006 <121087528+Afzal0006@users.noreply.github.com>
2025-08-24 20:13:36 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,8 @@ def main():
     app = Application.builder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     print("Bot is running...")
-    app.run_polling()
+    # Fix for Heroku RuntimeError: Event loop is closed
+    app.run_polling(close_loop=False)
 
 if __name__ == "__main__":
     main()
